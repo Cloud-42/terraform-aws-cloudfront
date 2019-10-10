d32005facab64a36d72c5af10a62957494dd1ad7
Cloud42 <cjmorgan@outlook.com>
2019-10-10 14:31:15 +0000

    initialising with bare bones TF

---
+++ README.md
@@ -0,0 +1,2 @@
+# terraform-aws-cloudfront
+Creates a CloudFront distribution.
---
+++ variables.tf
@@ -0,0 +1,174 @@
+variable "ssl_support_method" {
+  description = ""
+  default     = ""
+}
+
+variable "web_acl_id" {
+  description = ""
+  default     = ""
+}
+
+variable "enabled" {
+  description = ""
+  default     = ""
+}
+
+variable "default_ttl" {
+  description = ""
+  default     = ""
+}
+
+variable "geo_restriction_type" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_read_timeout" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_protocol_policy" {
+  description = ""
+  default     = ""
+}
+
+variable "viewer_minimum_protocol_version" {
+  description = ""
+  default     = ""
+}
+
+variable "viewer_protocol_policy" {
+  description = ""
+  default     = ""
+}
+
+variable "allowed_methods" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_https_port" {
+  description = ""
+  default     = ""
+}
+
+variable "compress" {
+  description = ""
+  default     = ""
+}
+
+variable "aliases" {
+  description = ""
+  default     = ""
+}
+
+variable "is_ipv6_enabled" {
+  description = ""
+  default     = ""
+}
+
+variable "min_ttl" {
+  description = ""
+  default     = ""
+}
+
+variable "max_ttl" {
+  description = ""
+  default     = ""
+}
+
+variable "forward_query_string" {
+  description = ""
+  default     = ""
+}
+
+variable "orchestration" {
+  description = ""
+  default     = ""
+}
+
+variable "comment" {
+  description = ""
+  default     = ""
+}
+
+variable "acm_certificate_arn" {
+  description = ""
+  default     = ""
+}
+
+variable "environment" {
+  description = ""
+  default     = ""
+}
+
+variable "geo_restriction_locations" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_keepalive_timeout" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_domain_name" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_path" {
+  description = ""
+  default     = ""
+}
+
+variable "cached_methods" {
+  description = ""
+  default     = ""
+}
+
+variable "forward_headers" {
+  description = ""
+  default     = ""
+}
+
+variable "price_class" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_http_port" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_ssl_protocols" {
+  description = ""
+  default     = ""
+}
+
+variable "forward_cookies" {
+  description = ""
+  default     = ""
+}
+
+variable "cache_behavior" {
+  description = ""
+  default     = ""
+}
+
+variable "default_root_object" {
+  description = ""
+  default     = ""
+}
+
+variable "origin_id" {
+  description = ""
+  default     = ""
+}
+
+variable "forward_cookies_whitelisted_names" {
+  description = ""
+  default     = ""
+}
